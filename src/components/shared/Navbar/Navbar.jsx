9b63e452823e0394a04c3c50e88dd570482f83a6
web-mahadihasan <mehedihasanmilu7@gmail.com>
2025-01-05 18:06:15 +0600

    add banner resume button

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
             <div className="container mx-auto px-4 xl:px-0 flex items-center justify-between">
                 {/* Logo  */}
                 <div>
-                    <Link to={"/"}><img src="" alt="Stay Rooms" className="h-10 md:h-12"/></Link>
+                    <Link to={"/"} className="text-red-500"><img src="" alt="Mahadi Hasan" className="h-10 md:h-12"/>Mahadi Hasan</Link>
                 </div>
 
                 {/* Nav links  */}
@@ -30,25 +30,7 @@ const Navbar = () => {
 
                 {/* Action button  */}
                 <div className="flex items-center gap-2">
-                    {
-                        loginUser ? <div className="flex items-center gap-3">
-                            <span className="p-2 border bg-white rounded-full cursor-pointer">test abc</span>
-                            {/* <img src="" alt="" className="w-10 h-10 rounded-full"/> */}
-                            <details className="dropdown dropdown-end">
-                                <summary className="btn p-0 bg-transparent hover:bg-transparent rounded-full flex items-center gap-2 border-gray-300 px-2">
-                                    <img src={user?.photoURL} alt="" className="w-9 h-9 rounded-full ring-2 ring-offset-2 ring-gray-100"/>
-                                    <IoIosArrowDown size={20} />
-                                </summary>
-                                <ul className="menu dropdown-content bg-white z-[1] p-2 min-w-60 space-y-2 border shadow-md rounded-md border-t-4 border-t-primary mt-3">
-                                    {/* <CurrentUser/> */}
-                                </ul>
-                            </details>
-                        </div> : (<div className="hidden font-medium md:flex items-center">
-                        <Link to={"/auth/login"}><button className="px-5 py-1.5 bg-white text-primary rounded shadow-md border border-primary hover:bg-primary hover:text-white hover:border-primary duration-500">Login</button></Link>
-                        <div className="mx-2 text-sm font-medium">OR</div>
-                        <Link to={"/auth/register"}><button className="px-5 py-1.5 bg-primary text-white rounded shadow-xl border border-primary hover:bg-secondary-black hover:border-secondary-black duration-500 tracking-wide">Register</button></Link>
-                    </div>)
-                    }
+                <Link to={"/"}><button className="px-5 py-1.5 bg-primary text-white rounded shadow-xl border border-primary hover:bg-secondary-black hover:border-secondary-black duration-500 tracking-wide">Download Resume</button></Link>
                     {/* Hamburger  */}
                     <div className="lg:hidden"><HiMenu size={30} /></div>
                     <aside>
