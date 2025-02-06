70727058dbbf2581a0b5d762cdc679f9c9b5c7bb
web-mahadihasan <mehedihasanmilu7@gmail.com>
2025-02-06 20:54:38 +0600

    Change animation style

--- src/components/common/Banner.jsx
+++ src/components/common/Banner.jsx
@@ -17,7 +17,7 @@ const Banner = () => {
                     <motion.p 
                         initial={{ opacity: 0, y: -80 }}
                         animate={{ opacity: 1, y: 0 }} 
-                        transition={{ duration: 0.8, ease: "linear" }}
+                        transition={{delay:0.1, duration: 0.8, ease: "linear" }}
                     className="text-2xl font-jost font-semibold">Hi 👋, I&apos;m Mahadi Hasan  </motion.p>
                     <motion.h1 className=" flex flex-col gap-2 font-extrabold font-rubik text-primary-black my-8"
                     initial={{ opacity: 0, y: 80 }}
